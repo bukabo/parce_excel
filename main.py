10b13139c7df8b1e01e7992ea631178eed1bec32
Merc <merc.tm@gmail.com>
2022-07-31 20:38:10 +0300

    test def

--- main.py
+++ main.py
@@ -1,3 +1,36 @@
 import pandas as pd
+import openpyxl
 
-pass
+pd.set_option('display.max_rows', 500)
+pd.set_option('display.max_columns', 500)
+pd.set_option('display.width', 1000)
+
+file_1 = './excel_files/Пример №1.xlsx'
+file_2 = './excel_files/Пример №2.xlsx'
+file_3 = './excel_files/Пример_3_20201022.xlsx'
+
+
+
+def test1():
+    df = pd.read_excel(file_1, sheet_name='Светофор №5', skiprows=4, nrows=18, usecols='A:R')
+    df.dropna(inplace=True)
+    return df
+
+
+def test2():
+    wb = openpyxl.load_workbook(filename=file_1, read_only=True)
+    ws = wb['Светофор №5']
+    data_rows = []
+    for row in ws['B7':'R78']:
+        data_cols = []
+        for cell in row:
+            data_cols.append(cell.value)
+        data_rows.append(data_cols)
+    df = pd.DataFrame(data_rows)
+    return df
+
+
+if __name__ == '__main__':
+    print(test1().head())
+    print('-' * 30)
+    print(test2().head())
