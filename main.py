a930c2b47c99e638b3c26e212f7c8f7d5b48c7c8
aleksandrovdv_dew2 <AleksandrovDV@fpc.org.rzd>
2022-08-02 15:11:49 +0300

    обработан 1-й файл

--- main.py
+++ main.py
@@ -12,63 +12,53 @@ file_2 = './excel_files/Пример №2.xlsx'
 file_3 = './excel_files/Пример_3_20201022.xlsx'
 
 
-def sample_1():
-    df = pd.read_excel(file_1, sheet_name='Светофор №5', skiprows=0, usecols='A:R')
-    df_names = df.head(2)
-    df_data = df.loc[5:]
-    df_n = df_names.T
-    df_n.ffill(axis=0, inplace=True)
-    df_n.fillna('', inplace=True)
-
-    df_n['name'] = df_n[0] + ' ' + df_n[1]
-    col_one_list = df_n['name'].tolist()
-
-    print(col_one_list)
-    df_data.columns = col_one_list
-    print(df)
-    print(df_data)
-    # df.dropna(inplace=True)
-    # return df
+def get_data_from_range():
+    pass
 
 
-def sample_2():
-    wb = openpyxl.load_workbook(filename=file_1, read_only=True)
+def sample_1(file, start_range='A1', end_range='B2'):
+    """
+    Функция получает данные из диапазона в excel файле, 'схлопывает' шапку и преобразует данные в плоскую таблицу.
+
+    :param file: исходный excel файл
+    :param start_range: стратовая ячейка диапазона
+    :param end_range: конечная ячейка диапазона
+    :return: итоговый DataFrame
+    """
+    wb = openpyxl.load_workbook(filename=file, read_only=True)
     ws = wb['Светофор №5']
     data_rows = []
-    for row in ws['B2':'R78']:
+    for row in ws[start_range:end_range]:
         data_cols = []
         for cell in row:
             data_cols.append(cell.value)
         data_rows.append(data_cols)
     df = pd.DataFrame(data_rows)
+
+    # получаем шапку из столбцов для преобразования
     df_names = df.head(3)
 
+    # получаем данные
     df_data = df.loc[4:]
-    df_n = df_names.T
-    df_n.ffill(axis=0, inplace=True)
-    df_n.fillna('', inplace=True)
 
-    df_n['name'] = df_n[0] + ' ' + df_n[1] + ' (' + df_n[2] + ')'
-    # df_n['name'] = df[[0, 1, 2]].agg(" ".join, axis=1)
-    df_n['name'].replace(to_replace=r'\n', value='', inplace=True, regex=True)
-    # df_n['name'].replace(to_replace=r'  ()', value='', inplace=True, regex=True)
-    # print(df_n)
-    # exit()
+    # транспонируем шапку, заполняем значения вниз, объединяем в один столбец (будущие заголовки)
+    df_names = df_names.T
+    df_names.ffill(axis=0, inplace=True)
+    df_names.fillna('', inplace=True)
+    df_names['name'] = df_names[0] + ' ' + df_names[1] + ' (' + df_names[2] + ')'
+    df_names['name'].replace(to_replace=r'\n', value='', inplace=True, regex=True)
 
-    col_one_list = df_n['name'].tolist()
-    # print(df_data)
-    # exit()
+    # собираем заголовки в список, применяем новые заголовки к датафрейму с данными
+    col_one_list = df_names['name'].tolist()
     df_data.columns = col_one_list
-    # print(df)
+
+    # анпивот датафрейма с данными
     df_unpivot = pd.melt(df_data, id_vars=col_one_list[0], value_vars=col_one_list[1:],
                          var_name='Params', value_name='Values')
     df_unpivot.rename(columns={col_one_list[0]: "Subject"}, inplace=True)
-    df_unpivot['time_stamp'] = datetime.now()
-    print(df_unpivot)
 
-    df_unpivot.to_csv('out.csv', index=False, sep='|')
-    # print(df)
-    return df
+    df_unpivot['time_stamp'] = datetime.now()
+    return df_unpivot
 
 
 def sample_3():
@@ -85,6 +75,5 @@ def sample_3():
 
 
 if __name__ == '__main__':
-    sample_2()
-    print('-' * 30)
-    # print(sample_3().head(10))
+    print(sample_1(file_1, start_range='B2', end_range='R78'))
+    print(sample_1(file_1, start_range='B84', end_range='R96'))
