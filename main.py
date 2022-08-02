a496377849752add8c61f85fb66fd706abf3b595
Denis_FPCUSER <nonames@bk.ru>
2022-08-02 20:54:27 +0300

    second file

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from parce_excel import *
 import pandas as pd
 
-pd.set_option('display.max_rows', 500)
 pd.set_option('display.max_columns', 500)
+pd.set_option('display.max_rows', 500)
 pd.set_option('display.width', 1000)
 pd.set_option('max_colwidth', 520)
 
